f6e480b01bac01c7865633344e8ca57540e5a539
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-07-09 19:52:17 +0100

    first

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController {
     }
 
     public function index() {
-        global $con, $title;
+        global $co, $title;
         $title = "Dashboard";
         $phone = $_SESSION['user_phone'] ?? null;
         $user = null;
